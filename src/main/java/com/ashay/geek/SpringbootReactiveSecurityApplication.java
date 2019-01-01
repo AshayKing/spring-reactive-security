6f2a7cd54a6a34872cc26b106c07a6fe827e850c
ashay.patil <ashay.patil@pubmatic.com>
2019-01-01 21:09:05 +0530

    initial setup commit

---
+++ src/main/java/com/ashay/geek/SpringbootReactiveSecurityApplication.java
@@ -0,0 +1,14 @@
+package com.ashay.geek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootReactiveSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootReactiveSecurityApplication.class, args);
+	}
+
+}
+
